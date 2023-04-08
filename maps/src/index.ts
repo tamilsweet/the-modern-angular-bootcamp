3b52cb5388c3727d83588b0b3f0d623e9316e272
Tamilmozhi Gunasekar <tgunasekar@rbbn.com>
2023-04-08 10:23:15 +0530

    Add user and company to google map Random data generated for user and company

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,13 @@
+/// <reference types="@types/google.maps" />
 console.log('Hello World!');
+
+import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
+import { User } from "./User";
+
+const customMap = new CustomMap("map");
+const user = new User();
+const company = new Company();
+
+customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
