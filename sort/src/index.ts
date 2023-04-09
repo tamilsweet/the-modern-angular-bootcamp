2d01fc78fc0d6f5cbc850a55201aee7d89ac3f9f
Tamilmozhi Gunasekar <tgunasekar@rbbn.com>
2023-04-09 10:33:23 +0530

    Add linked list class for sorting

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numCollection);
@@ -11,3 +12,15 @@ const strCollection = new CharactersCollection('zyx');
 const sorter1 = new Sorter(strCollection);
 sorter1.sort();
 console.log(strCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(7);
+linkedList.add(2);
+
+linkedList.print();
+
+const sorter2 = new Sorter(linkedList);
+sorter2.sort()
+linkedList.print();
